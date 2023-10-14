d868ca4716d87079531b446309833654c287a8e6
Romano Tio <72582417+rat83@users.noreply.github.com>
2023-10-13 22:58:09 -0400

    Defined submodules in prep for basic boid accelerator

--- VGA_to_M10k/verilog/boid_accelerator/boid_accelerator.sv
+++ VGA_to_M10k/verilog/boid_accelerator/boid_accelerator.sv
@@ -1,31 +1,121 @@
 module boid_accelerator(
-	input logic clk;
-	input logic reset;
+	input logic clk,
+	input logic reset,
 	
 	output logic [31:0]  
 		x,  y, 	// current position
 		px, py, 	// previous position (for erasure)
-		vx, vy; 	// velocity
+		vx, vy 	// velocity
 	
 );
+	/*
+		This block retains state of x, y, vx, and vy. Every cycle it is clocked for, it
+		will latch in updated values for x, y, vx, vy that should simply move the 
+		
+		ok this documentation is shit update it later. its just gonna move the boid
+		around and make it turn at the boundary
+	*/
 
+	// x/v transient wires
+	
+	logic [31:0] x_comb, 	y_comb
+					 xv_comb,	yv_comb;
+	
 
-
-	// speed update
+	// pos/speed regs
+	
+	d_reg x_reg #(32,(32'd180 << 16))
+	(
+		.clk		(clk),
+		.reset	(reset),
+		.d			(x_comb),
+		.q			(x)
+	);
+	
+	d_reg y_reg #(32,(32'd240 << 16))
+	(	
+		.clk		(clk),
+		.reset	(reset),
+		.d			(y_comb),
+		.q			(y)
+	);
 	
 	
-
+	d_reg vx_reg #(32,(32'd4 << 16))
+	(
+		.clk		(clk),
+		.reset	(reset),
+		.d			(vx_comb),
+		.q			(vx)
+	);
+	
+	d_reg vy_reg #(32,(32'd4 << 16))
+	(
+		.clk		(clk),
+		.reset	(reset),
+		.d			(vx_comb),
+		.q			(vx)
+	);
+	
+	// margins
+	
+	logic [31:0] x_bound, y_bound;
+	
+	d_reg x_bound_reg #(32,(32'd100 << 16))
+	(
+		.clk		(clk),
+		.reset	(reset),
+		.d 		(x_bound),
+		.q			(x_bound)
+	);
+	
+	d_reg y_bound_reg	#(32,(32'd100 << 16))
+	(
+		.clk		(clk),
+		.reset	(reset),
+		.d 		(y_bound),
+		.q			(y_bound)
+	);
+	
+	logic [31:0] speed;
+	logic [31:0] vx_sq, vy_sq;
+	
+	// speed calculation
+	
+	fix15_mul vx_mul
+	(
+		.a		(vx),
+		.b		(vx),
+		.q		(vx_sq)
+	);
+	
+	fix15_mul vy_mul
+	(
+		.a		(vy),
+		.b		(vy),
+		.q		(vy_sq)
+	);
+	
+	amax_bmin speed_calc
+	(
+		.a		(vx_sq),
+		.b		(vy_sq),
+		.q		(speed)
+	);
+	
+	
+	
 endmodule
 
 module d_reg
 	#(
-		parameter d_width = 32;
-		parameter d_res_v = 0;
+		parameter d_width = 32,
+		parameter d_res_v = 0
 	)(
-	input logic 	clk;
-	input logic 	reset;
-	input logic 	[d_width-1:0] d;
-	output logic 	[d_width-1:0] q;
+	input logic 	clk,
+	input logic 	reset,
+	input logic 	[d_width-1:0] d,
+	output logic 	[d_width-1:0] q
 	);
 	
 	always @(posedge clk) begin
@@ -39,18 +129,18 @@ module d_reg
 endmodule
 
 module fix15_mul(
-	input logic 	[31:0] a, b;
-	output logic	[31:0] q;
+	input logic 	[31:0] a, b,
+	output logic	[31:0] q
 	);
-	
-	assign logic [63:0] temp = a * b;
+	logic [63:0] temp;
+	assign temp = a * b;
 	assign q = temp[47:15];
 	
 endmodule
 
 module amax_bmin(
-	input logic 	[31:0] a, b;
-	output logic	[31:0] q;
+	input logic 	[31:0] a, b,
+	output logic	[31:0] q
 	);
 	
 	logic [31:0] a_temp, b_temp;
--- VGA_to_M10k/verilog/boid_accelerator/testbench.sv
+++ VGA_to_M10k/verilog/boid_accelerator/testbench.sv
@@ -9,21 +9,115 @@ always begin
 	clk = !clk;
 end
 
-logic [3:0] num;
-logic [6:0] segs;
+logic [31:0] a_1, b_1, c_1, q_1, a_2, b_2, c_2, q_2;
+
+// I'm aware this testbench is kinda gross right now, these are mostly 'sanity check'
+// type tests to make sure the modules I instantiated actually work properly
+
+// checking amax_bmin
+initial begin
+	a_1 = 32'h0000c000;
+	b_1 =	32'h00013333;
+	c_1 =	32'h00019333;
+	#50
+	assert (c_1 == q_1);
+	a_1 = 32'h0001d999;
+	b_1 = 32'h00029999;
+	c_1 = 32'h00038665;
+	#50
+	assert (c_1 == q_1);
+	a_1 = 32'h0004f333;
+	b_1 = 32'h00014000;
+	c_1 = 32'h00059333;
+	#50
+	assert (c_1 == q_1);
+	a_1 = 32'h011c10e6;
+   b_1 = 32'h00de1f1b;
+   c_1 = 32'h018b2073;
+   #50;
+	assert (c_1 == q_1);
+	a_1 = 32'h0071244e;
+   b_1 = 32'h0000004b;
+   c_1 = 32'h00712473;
+   #50;
+   assert (c_1 == q_1);
+	a_1 = 32'h00566666;
+   b_1 = 32'h09c3f330;
+   c_1 = 32'h09ef2663;
+   #50;
+   assert (c_1 == q_1);
+end
+
+// checking fix15_mul
+initial begin
+	a_2 = 32'h00566666;
+   b_2 = 32'h09c3f330;
+   c_2 = 32'h97775230;
+   #50;
+	assert (c_2 == q_2);
+	a_2 = 32'h00000013;
+   b_2 = 32'h01199c28;
+   c_2 = 32'h000029cd;
+   #50;
+   assert (c_2 == q_2);
+	a_2 = 32'hffffffff;
+	b_2 = 32'hffffffff;
+	c_2 = 32'hFFFC0000;
+   #50;
+   assert (c_2 == q_2);
+	
+end
 
 initial begin
-	clk = 1'b0;
-	num = 4'b0;
+	#5000
+	$stop;
 end
 
 always @(posedge clk) begin
-	num <= num + 4'd1;
+	// num <= num + 4'd1;
 end
 
-HexDigit dut ( 
-	.*
+amax_bmin dut1 (
+	.a(a_1),
+	.b(b_1),
+	.q(q_1)
 );
 
+fix15_mul dut2(
+	.a(a_2),
+	.b(b_2),
+	.q(q_2)
+);
 
 endmodule
+
+/*
+
+C model checking verilog helper modules against:
+
+#include <stdio.h>
+
+typedef signed int fix15 ;
+#define multfix15(a,b) ((fix15)((((signed long long)(a))*((signed long long)(b)))>>15))
+#define float2fix15(a) ((fix15)((a)*32768.0)) // 2^15
+#define fix2float15(a) ((float)(a)/32768.0)
+#define absfix15(a) abs(a) 
+#define int2fix15(a) ((fix15)(a << 15))
+#define fix2int15(a) ((int)(a >> 15))
+#define char2fix15(a) (fix15)(((fix15)(a)) << 15)
+#define divfix(a,b) (fix15)(( (((signed long long)(a)) << 15) / ((signed long long)(b))))
+
+fix15 alpha_max_beta_min(fix15 a, fix15 b){
+    fix15 max = (a > b) ? a : b;
+    fix15 min = (a > b) ? b : a;
+    
+    return(max + (min >> 1));
+}
+
+int main(){
+	// call alpha_max_beta_min on values
+	// print input and output values out as hex
+	// use those values to check against output from Verilog
+}
+
+*/
